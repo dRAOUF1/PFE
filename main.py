5e470a9948bfe03cab4d7d7b05953bd16774fd0b
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-03-16 18:33:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from App import App
-import cv2 ,time
+import cv2 ,time, os
 import requests
 import sys,getopt
+from picamera2 import Picamera2
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def usage():
   print("Utilisation : {} -i <adresse_ip> -p <port> [-r <degre_rotation>]".format(sys.argv[0]))
   print("Options :")
   print("  -i <adresse_ip>  : Adresse IP du back")
-  print("  -p <port>  : port")
-  print("  -r <degre_rotation> : DegrÃ© de rotation de l'image (0, 90, 180 ou 270)")
+  print("  -r <degre_rotation> : DegrÃƒÂ© de rotation de l'image (0, 90, 180 ou 270)")
   print("  -s <save faces> : Enregistrer de nouvelles images des visage (0 ou 1)")
 
 
@@ -24,14 +26,14 @@ rotations = {0:(None,(640,360 )),
 
 if __name__ == '__main__':
     
-    adresse_ip = None
-    port = None
-    degre_rotation = 0
+    adresse_ip = os.getenv('URL_BACKEND')
+    port = os.getenv('PORT')
+    degre_rotation = int(os.getenv('ROTATION'))
     save_faces = False
 
     # Analyser les arguments de la ligne de commande
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "i:r:p:")
+        opts, args = getopt.getopt(sys.argv[1:], "ir")
     except getopt.GetoptError as err:
         print(err)
         usage()
@@ -43,8 +45,6 @@ if __name__ == '__main__':
           adresse_ip = arg
         elif opt in ("-r", "--rotation"):
           degre_rotation = int(arg)
-        elif opt in ("-p", "--port"):
-            port = arg
         elif opt in ("-s"):
             if opt == 1:
                 save_faces = True
@@ -54,81 +54,81 @@ if __name__ == '__main__':
           usage()
           sys.exit(1)
 
-    # VÃ©rifier le degrÃ© de rotation
+    # VÃƒÂ©rifier le degrÃƒÂ© de rotation
     if degre_rotation not in (0, 90, 180, 270):
-        print("Degré de rotation invalide : {}".format(degre_rotation))
+        print("DegrÃ© de rotation invalide : {}".format(degre_rotation))
         usage()
         sys.exit(1)
     else:
         degre_rotation = rotations[degre_rotation]
-    if adresse_ip == None:
-        print("Adresse ip invalide")
-        sys.exit(1)
-    if port == None:
-        print("port invalide")
-        sys.exit(1)
     
-    app = App("http://localhost:3001/getEmbeddings/34/1")
+    
+    app = App(f"http://{adresse_ip}/getEmbeddings/all/all")
+
     print("fin app build")
     presents = []
 
-    cap = cv2.VideoCapture(0)
+    # Create an instance of the PiCamera2 object
+    cam = Picamera2()
+    # Set the resolution of the camera preview
+    cam.preview_configuration.main.size = degre_rotation[1]
+    cam.preview_configuration.main.format = "RGB888"
+    cam.preview_configuration.controls.FrameRate=60
+    cam.preview_configuration.align()
+    cam.configure("preview")
+    cam.start()
+    
     prev_frame_time = 0
     fp = []
     c = 0
     print("debut boucle")
-    # while True:
-        # ret,frame=cap.read()
-        # if not ret:
-        #     break
+    while True:
+#         ret,frame=cap.read()
+#         if not ret:
+#             break
         #print("capture")
-    frame = cv2.imread("C:/Users/yas/Desktop/abadlijpg.jpg")
-    #resize frame to 0.5
-    frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
-    if not degre_rotation[0] == None:
-        frame = cv2.rotate(frame, degre_rotation[0])
-    
-    c+=1
-    # frame = cv2.resize(frame,(128,96))
-    if c%1==0:
-        faces = app.find_match(frame)
+        frame = cam.capture_array()
+        #resize frame to 0.5
+        #frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
+        if not degre_rotation[0] == None:
+            frame = cv2.rotate(frame, degre_rotation[0])
+        
+        c+=1
+        # frame = cv2.resize(frame,(128,96))
+        if c%5==0:
+            faces = app.find_match(frame)
 
-        #print(len(faces),"hadi len of faces")
-        if len(faces)>0:
-            for face in faces:
-                if save_faces:
-                    cropped = frame[int(face.face.y1):int(face.face.y2), int(face.face.x1):int(face.face.x2)]
-                    cropped = cv2.resize(cropped,(128,128))
-                    cv2.imwrite(f"faces/{face.name}-{time.time()}.jpg",cropped)
-                frame = app.Draw(frame,face)
-                # print("\n",face.name,matricule[face.name] )
-                print(face.name)    
-                if face.name not in presents:
-                    presents.append(face.name)
-                    r = requests.post(f"http://{adresse_ip}:{port}/postEtdsPresent",json={"matricule":face.name})
+            #print(len(faces),"hadi len of faces")
+            if len(faces)>0:
+                for face in faces:
+                    if save_faces:
+                        cropped = frame[int(face.face.y1):int(face.face.y2), int(face.face.x1):int(face.face.x2)]
+                        cropped = cv2.resize(cropped,(128,128))
+                        cv2.imwrite(f"faces/{face.name}-{time.time()}.jpg",cropped)
+                    frame = app.Draw(frame,face)
+                    # print("\n",face.name,matricule[face.name] )
+                    print(face.name, face.distance)    
+                    r = requests.post(f"http://{adresse_ip}/postEtdsPresent",json={"matricule":face.name})
                     print(r)
-                    #exit(0)
-                
-                
-        #else:
-            #   print("walo")
-                
-        # else:
-        #     print('unknown')
-    
-    new_frame_time = time.time()
-    try: 
-        fps = 1/(new_frame_time-prev_frame_time) 
-    except (ZeroDivisionError):
-        pass
-    prev_frame_time = new_frame_time 
-    fp.append(fps)
-    fps = str(int(fps))
-    #cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)  
-    cv2.imshow("lol",frame)
-    k = cv2.waitKey(0)
-        # if k == 27:         # wait for ESC key to exit
-        #     break
+                        #exit(0)
+                    
+                    
+            #else:
+                #   print("walo")
+                    
+            # else:
+            #     print('unknown')
+        
+        new_frame_time = time.time()
+        try: 
+            fps = 1/(new_frame_time-prev_frame_time) 
+        except (ZeroDivisionError):
+            pass
+        prev_frame_time = new_frame_time 
+        fp.append(fps)
+        fps = str(int(fps))
+        #cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA) 
+        
         
 
     #average of fp
@@ -141,4 +141,6 @@ if __name__ == '__main__':
         pass
     #cv2.destroyAllWindows()
     
+
+
 
