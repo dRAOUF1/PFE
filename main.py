d5bacb1acfe24449d19fce188d4eed3585b21cb7
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-04-05 15:09:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from App import App
 import cv2 ,time, os
 import requests
 import sys,getopt
-<<<<<<< Updated upstream
 from picamera2 import Picamera2
 from dotenv import load_dotenv
 
@@ -16,46 +15,26 @@ def usage():
   print("  -s <save faces> : Enregistrer de nouvelles images des visage (0 ou 1)")
 
 
-=======
+
 from centroidtracker2 import CentroidTracker
 from VideoStream import VideoStream
 import numpy as np
-from scipy.signal import convolve2d as conv2
->>>>>>> Stashed changes
-
-from skimage import color, data, restoration
-
-def richardson_lucy_blind(image, psf, num_iter=50):    
-    psf = np.ones((5, 5)) / 25
-    rng = np.random.default_rng()
-    # astro = conv2(image, psf, 'same')
-    # Add Noise to Image
-    
-
-    # Restore Image using Richardson-Lucy algorithm
-    deconvolved_RL = restoration.richardson_lucy(image, psf, num_iter=30)   
-    return deconvolved_RL
 
 
-rotations = {0:(None,(640,360 )),
-             90:(cv2.ROTATE_90_CLOCKWISE,(360,640 )),
-             180:(cv2.ROTATE_180,(640,360 )),
-             270:(cv2.ROTATE_90_COUNTERCLOCKWISE,(360,640 ))
+
+rotations = {0:(None,(1920,1080)),
+             90:(cv2.ROTATE_90_CLOCKWISE,(1080,1920 )),
+             180:(cv2.ROTATE_180,(1920,1080)),
+             270:(cv2.ROTATE_90_COUNTERCLOCKWISE,(1080,1920 ))
     }
 
 
 if __name__ == '__main__':
-<<<<<<< Updated upstream
+
     
     adresse_ip = os.getenv('URL_BACKEND')
     port = os.getenv('PORT')
     degre_rotation = int(os.getenv('ROTATION'))
-=======
-
-    adresse_ip = None
-    port = None
-    degre_rotation = 0
->>>>>>> Stashed changes
     save_faces = False
 
     
@@ -96,7 +75,7 @@ if __name__ == '__main__':
     print("fin app build")
     presents = []
 
-<<<<<<< Updated upstream
+
     # Create an instance of the PiCamera2 object
     cam = Picamera2()
     # Set the resolution of the camera preview
@@ -107,75 +86,21 @@ if __name__ == '__main__':
     cam.configure("preview")
     cam.start()
     
-=======
-    # cap = VideoStream("C:/Users/yas/Downloads/archive (3)/20240228_192336 - abdeljalil abed.mp4")
-    cap = VideoStream("C:/Users/yas/Desktop/test.avi")
-    cap = VideoStream(0)
-    cap.start()
->>>>>>> Stashed changes
     prev_frame_time = 0
     fp = []
     c = 0
     print("debut boucle")
     while True:
-<<<<<<< Updated upstream
-#         ret,frame=cap.read()
-#         if not ret:
-#             break
-        #print("capture")
-        frame = cam.capture_array()
-        #resize frame to 0.5
-        #frame = cv2.resize(frame, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_AREA)
-        if not degre_rotation[0] == None:
-            frame = cv2.rotate(frame, degre_rotation[0])
-        
-        c+=1
-        # frame = cv2.resize(frame,(128,96))
-        if c%5==0:
-            faces = app.find_match(frame)
-
-            #print(len(faces),"hadi len of faces")
-            if len(faces)>0:
-                for face in faces:
-                    if save_faces:
-                        cropped = frame[int(face.face.y1):int(face.face.y2), int(face.face.x1):int(face.face.x2)]
-                        cropped = cv2.resize(cropped,(128,128))
-                        cv2.imwrite(f"faces/{face.name}-{time.time()}.jpg",cropped)
-                    frame = app.Draw(frame,face)
-                    # print("\n",face.name,matricule[face.name] )
-                    print(face.name, face.distance)    
-                    r = requests.post(f"http://{adresse_ip}/postEtdsPresent",json={"matricule":face.name})
-                    print(r)
-                        #exit(0)
-                    
-                    
-            #else:
-                #   print("walo")
-                    
-            # else:
-            #     print('unknown')
-=======
         rects = []
         frame=cap.read()
         if frame is None:
             break
         
-        # if not degre_rotation[0] == None:
-        #     frame = cv2.rotate(frame, degre_rotation[0])
+        if not degre_rotation[0] == None:
+            frame = cv2.rotate(frame, degre_rotation[0])
         
         c+=1
-        # frame = cv2.resize(frame,None,fx=1.1,fy=1.1,interpolation=cv2.INTER_AREA)
-        # frame = cv2.resize(frame,(320,320),interpolation=cv2.INTER_AREA)
-        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # gamma = 1.5
-        # lookup_table = np.array([((i / 255.0) ** gamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
-        # frame = cv2.LUT(frame, lookup_table)
-        # frame, alpha, beta = automatic_brightness_and_contrast(frame)
-        # recon = richardson_lucy_blind(frame,9)
-        
-
-        if c%1==0:
+        if c%2==0:
             
             faces = app.find_match(frame)
             for face in faces:
@@ -191,8 +116,6 @@ if __name__ == '__main__':
                 # r = requests.post(f"http://{adresse_ip}:{port}/postEtdsPresent",json={"matricule":face.name})
                 # print(r)
                     #exit(0)
-
->>>>>>> Stashed changes
         
         new_frame_time = time.time()
         try: 
@@ -202,20 +125,12 @@ if __name__ == '__main__':
         prev_frame_time = new_frame_time 
         fp.append(fps)
         fps = str(int(fps))
-<<<<<<< Updated upstream
-        #cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA) 
-        
-        
-=======
-        #cv2.putText(frame, fps, (7, 70), cv2.FONT_HERSHEY_SIMPLEX, 3, (100, 255, 0), 3, cv2.LINE_AA)  
         cv2.imshow("lol",frame)
         # cv2.imshow("recon",recon)
         k = cv2.waitKey(1)
         if k == 27:         # wait for ESC key to exit
             break
             
->>>>>>> Stashed changes
-
     #average of fp
     c = 0
     for f in fp:
